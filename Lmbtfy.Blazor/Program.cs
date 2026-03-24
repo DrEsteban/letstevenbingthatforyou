0629aad69c31b385c150a96829dc2d5b315ba2d8
Steven Ryland <stevenry@outlook.com>
2026-03-24 09:54:55 -0700

    Switch to top-level statements

--- Lmbtfy.Blazor/Program.cs
+++ Lmbtfy.Blazor/Program.cs
@@ -1,28 +1,20 @@
 using Blazored.Modal;
+using Lmbtfy.Blazor;
 using Lmbtfy.Blazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Lmbtfy.Blazor
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddHttpClient("Zip1", client =>
-                client.BaseAddress = new Uri("https://zip1.io/"));
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("Zip1", client =>
+    client.BaseAddress = new Uri("https://zip1.io/"));
 
-            builder.Services.AddBlazoredModal();
+builder.Services.AddBlazoredModal();
 
-            builder.Services.AddHttpClient();
-            builder.Services.AddSingleton<IImageService, ImageService>();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<IImageService, ImageService>();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
